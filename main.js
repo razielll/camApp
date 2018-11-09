482f704be9360da27a99483c8d490ffd1d2e3dc7
Anton S <asto1387@gmail.com>
2018-11-09 14:47:22 +0200

    mac support? improved base algo and svg

--- main.js
+++ main.js
@@ -1,29 +1,58 @@
-// Set constraints for the video stream
-var constraints = { video: { facingMode: 'environment' }, audio: false };
 // Define constants
 const cameraView = document.querySelector('#camera--view'),
 	cameraOutput = document.querySelector('#camera--output'),
 	cameraSensor = document.querySelector('#camera--sensor'),
 	cameraTrigger = document.querySelector('#camera--trigger');
+
 // Access the device camera and stream to cameraView
 function cameraStart() {
+	const constraints = { video: { facingMode: 'environment' }, audio: false };
+	// Older browsers might not implement mediaDevices at all, so we set an empty object first
+	if (navigator.mediaDevices === undefined) {
+		navigator.mediaDevices = {};
+	}
+	if (navigator.mediaDevices.getUserMedia === undefined) {
+		navigator.mediaDevices.getUserMedia = constraints => {
+			// First get ahold of the legacy getUserMedia, if present
+			console.log('constraints:', constraints);
+			let getUserMedia =
+				navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+			// Some browsers just don't implement it - return a rejected promise with an error
+			// to keep a consistent interface
+			if (!getUserMedia) {
+				return Promise.reject(
+					new Error('getUserMedia is not implemented in this browser')
+				);
+			}
+
+			// Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+			return new Promise((resolve, reject) => {
+				getUserMedia.call(navigator, constraints, resolve, reject);
+			});
+		};
+	}
+
 	navigator.mediaDevices
 		.getUserMedia(constraints)
-		.then(function(stream) {
-			track = stream.getTracks()[0];
+		.then(stream => {
+			// track = stream.getTracks()[0];
 			cameraView.srcObject = stream;
 		})
-		.catch(function(error) {
+		.catch(error => {
 			console.error('Oops. Something is broken.', error);
 		});
 }
+
 // Take a picture when cameraTrigger is tapped
-cameraTrigger.onclick = function() {
+cameraTrigger.onclick = () => {
 	cameraSensor.width = cameraView.videoWidth;
 	cameraSensor.height = cameraView.videoHeight;
 	cameraSensor.getContext('2d').drawImage(cameraView, 0, 0);
 	cameraOutput.src = cameraSensor.toDataURL('image/webp');
 	cameraOutput.classList.add('taken');
+	cameraView.srcObject.getVideoTracks().forEach(track => track.stop());
 };
+
 // Start the video stream when the window loads
 window.addEventListener('load', cameraStart, false);
